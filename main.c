c4c1a62435acc8e8e18c18f3c428a49e6cc47e3b
Theo-Rennolds <theo2.rennolds@live.uwe.ac.uk>
2026-04-27 14:33:35 +0100

    Attempt to open, read and save data. Failed. Find the correct path to the file, as it cannot be saved in the same folder.

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(void){
+    FILE *power_data = fopen("","r");
+    if (power_data == NULL){
+        printf("File is either empty or not found. \n");
+        return 1;
+    }
+    char timestamp[100];
+    char phase_A_voltage[100];
+    char phase_B_voltage[100];
+    char phase_C_voltage[100];
+    char line_current[100];
+    char frequency[100];
+    char power_factor[100];
+    char trd_percent[100];
+
+    while (fscanf(power_data,"%lf %lf %lf %lf %lf %lf %lf %lf",&timestamp,
+                  &phase_A_voltage,&phase_B_voltage,&phase_C_voltage,
+                  &line_current,frequency,power_factor,trd_percent) == 8) {
+        printf("time stamp: %lf", timestamp);
+    }
+
+    fclose(power_data);
     return 0;
 }
